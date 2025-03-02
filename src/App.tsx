175e010a64182cc905136e28b6122731181431d1
GolamKIBRI4 <gk599625@gmail.com>
2025-03-02 10:57:09 +0600

    Button Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { useState } from "react";
 function App() {
-  return <div>Hello Quasba</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const toggle = () => {
+    setIsLoading(!isLoading);
+  };
+  return (
+    <div>
+      {isLoading && (
+        <Button loading loadingText="loading">
+          Click me
+        </Button>
+      )}
+      <Button onClick={toggle}>Click Me!</Button>
+    </div>
+  );
 }
 
 export default App;

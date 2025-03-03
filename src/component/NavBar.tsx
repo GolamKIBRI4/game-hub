9abf646de611da008d6e7c09df0e1dba44b49674
GolamKIBRI4 <gk599625@gmail.com>
2025-03-03 16:05:49 +0600

    ImplementDarkMode

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/webpage.svg";
 import { px } from "framer-motion";
+import { ColorMode, ColorModeButton } from "../components/ui/color-mode";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize={"60px"} />
       <Text>NavBar</Text>
+      <ColorModeButton />
     </HStack>
   );
 };
